217295ba2dccefa95461cfac4cbe03c3ad920015
Naveenraj <>
2023-01-31 16:38:31 +0530

    no message

---
+++ Test1/src/test/java/test/BaseClass.java
@@ -0,0 +1,16 @@
+package test;
+
+
+
+public class BaseClass {
+
+	public void  UserName (){
+     		String user = "alit.edc@mailinator.com";
+		
+	}
+	
+	public void Password() {
+		String pass = "Hello@123";
+	}
+
+}
